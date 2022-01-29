d90e72bb28c349781d14d19d8ef09256a5a4cd9d
abelyem <abel.yemane1@hotmail.co.uk>
2022-01-29 15:58:35 +0000

    fixed indents and removed empty space (no changes made to code)

--- reed_home/views.py
+++ reed_home/views.py
@@ -6,17 +6,18 @@ load_dotenv()
 
 
 def home(request):
-     API_KEY = os.getenv("API_KEY")
-     API_PASS = os.getenv("API_PASS")
-     x=requests.get("https://www.reed.co.uk/api/1.0/search?keywords=junior+developer&locationName=london", auth=(API_KEY, API_PASS))
-     response= x.json()
-     if 'name' and 'locationName' in request.GET:
-        name = request.GET['name']
-        location = request.GET['locationName']
-        url = requests.get(f"https://www.reed.co.uk/api/1.0/search?keywords=junior+developer+{name}&locationName={location}", auth=(API_KEY, API_PASS))
-        response = url.json()
-        return render (request, 'job_details/details.html', {'response': response})
-     return render(request, 'reed_home/index.html', {'response': response})
+   API_KEY = os.getenv("API_KEY")
+   API_PASS = os.getenv("API_PASS")
+   x=requests.get("https://www.reed.co.uk/api/1.0/search?keywords=junior+developer&locationName=london", auth=(API_KEY, API_PASS))
+   response= x.json()
+   if 'name' and 'locationName' in request.GET:
+      name = request.GET['name']
+      location = request.GET['locationName']
+      url = requests.get(f"https://www.reed.co.uk/api/1.0/search?keywords=junior+developer+{name}&locationName={location}", auth=(API_KEY, API_PASS))
+      response = url.json()
+      return render (request, 'job_details/details.html', {'response': response})
+   return render(request, 'reed_home/index.html', {'response': response})
+ 
 
 
 
